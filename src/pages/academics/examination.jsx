daf1fb84ca7dba9688d31a0391edd9668ace3bfa
Arpitakharde <arpitakharde30@gmail.com>
2026-09-06 17:20:03 -0800

    website added for MBA time table

--- src/pages/academics/examination.jsx
+++ src/pages/academics/examination.jsx
@@ -74,7 +74,7 @@ const Examination = () => {
             <div className="w-52 border-b-2 border-[#0b2d4d] mt-3 mb-10"></div>
 
             <a
-              href="/pdfs/mba-timetable.pdf"
+              href="https://exam.unipune.ac.in/index.html"
               target="_blank"
               className="text-2xl text-blue-500"
             >
