ca7cda8837a794af4f4797a056fdb8346a9a995f
Arpitakharde <arpitakharde30@gmail.com>
2026-09-06 17:21:31 -0800

    website added for Performance Improvement Online Exam Form

--- src/pages/academics/examination.jsx
+++ src/pages/academics/examination.jsx
@@ -90,7 +90,7 @@ const Examination = () => {
             <div className="w-52 border-b-2 border-[#0b2d4d] mt-3 mb-10"></div>
 
             <a
-              href="/pdfs/performance-improvement.pdf"
+              href="https://exam.unipune.ac.in/Pages/ExamFormsOnline.html"
               target="_blank"
               className="text-2xl text-blue-500"
             >
